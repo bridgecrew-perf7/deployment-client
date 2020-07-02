43e36577f716c4a291a027b539378e7d15b0414c
Jacob Bushman <jacob.matthew.bushman@endurance.com>
2020-07-02 16:32:37 -0600

    systemd deployment

--- build/lib/dclient/resources/rollout.py
+++
@@ -1,9 +0,0 @@
-import falcon
-from dclient.controllers.rollout import *
-
-
-class Rollout(object):
-    def on_post(self, req, resp):
-        response = post_rollout(self, data=req.media)
-        resp.body = json.dumps(response["body"])
-        resp.status = response["status"]
