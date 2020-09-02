d8910333da02edff7bf5e117486183c0d9c84275
Jacob <jacob.matthew.bushman@endurance.com>
2020-09-02 10:02:52 -0600

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dclient",
-    version="0.0.104",
+    version="0.0.105",
     author="Jacob Bushman",
     author_email="jacob.matthew.bushman@endurance.com",
     description="Deployment Client API",
