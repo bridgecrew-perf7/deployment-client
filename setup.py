93fee5eadb80f44d763bcf76ecdaf29b28a8b0ba
Jacob <jacob.matthew.bushman@endurance.com>
2020-10-02 07:23:51 -0600

    increment setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dclient",
-    version="0.1.16",
+    version="0.1.17",
     author="Jacob Bushman",
     author_email="jacob.matthew.bushman@endurance.com",
     description="Deployment Client API",
