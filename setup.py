ebe69584a3e429db57bbe0d75c02c0572588747c
Jacob <jacob.matthew.bushman@endurance.com>
2020-11-18 15:51:56 -0700

    set test_deployment yum_transaction_id=1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dclient",
-    version="0.2.5",
+    version="0.2.6",
     author="Jacob Bushman",
     author_email="jacob.matthew.bushman@endurance.com",
     description="Deployment Client API",
