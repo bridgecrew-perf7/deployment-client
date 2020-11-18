43b5d5acf99af04032164896b29979c3c506cc2d
Jacob <jacob.matthew.bushman@endurance.com>
2020-11-18 12:40:49 -0700

    custom.repo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="dclient",
-    version="0.2.2",
+    version="0.2.3",
     author="Jacob Bushman",
     author_email="jacob.matthew.bushman@endurance.com",
     description="Deployment Client API",
