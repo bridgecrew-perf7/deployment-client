7f80c7648e96b87e8c9d1c52deeb885efae5ea43
Jacob <jacob.matthew.bushman@endurance.com>
2020-11-18 09:28:26 -0700

    remove rollout and rollback individual tests and put them in deployment

--- tests/integration_tests/test_rollout.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def test_post_rollout(base_url):
-    data = {"status": "good"}
-    response = requests.post(base_url + "/rollout", json=data)
-    print(response.text)
-    assert response.status_code == 201
